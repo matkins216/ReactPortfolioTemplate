879cd192a18057d157525d128fa38b7c3075c894
Matt <matt.atkins216@gmail.com>
2023-08-27 10:18:17 -0600

    first commit

--- src/info/Info.js
+++ src/info/Info.js
@@ -2,8 +2,7 @@ import self from "../img/self.png"
 import mock1 from "../img/mock1.png"
 import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -23,17 +22,17 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Matt",
+    lastName: "Atkins",
+    initials: "ma", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
+            emoji: '🥊',
+            text: 'fueled by muay thai'
         },
         {
             emoji: '🌎',
@@ -41,65 +40,50 @@ export const info = {
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Account Manager at Covene"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "matt.atkins216@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/matkins216",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/matthewatkins216/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Matt. I am an account manager at Covene, helping clients with their IT solution needs. I graduated the fullstack software engineer program at General Assembly. I also have my BA in Marketing from Carthage College. My end-user experience in SaaS allows me to have a unique perspective for software projects.",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['Javascript', 'React', 'Git', 'Github', 'MaterialUI', 'HTML5', 'CSS3', 'Figma', 'LucidChart', 'SemanticUI', 'SQL', 'Node.js', 'Express', 'MongoDB', 'Python', 'Django' ],
+            exposedTo: ['AWS', 'Azure', 'PostgresSQL']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'Muay Thai',
+            emoji: '🥊'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'Golf',
+            emoji: '⛳'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'DJing/Producing',
+            emoji: '🎛️'
         },
         {
-            label: 'cooking',
+            label: 'Cooking',
             emoji: '🌶'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
@@ -107,34 +91,22 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
-        },
-        {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock2
-        },
-        {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Recipe Book",
+            live: "https://odd-plum-clam-vest.cyclic.app/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/matkins216/recipe-book-REACT-FullStack", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock3
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
+            title: "Salon Keeper",
+            live: "https://grumpy-puce-narwhal.cyclic.app/",
+            source: "https://github.com/matkins216/salonkeeper",
+            image: mock2
         },
         {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
+            title: "Buck's BlackJack",
+            live: "https://matkins216.github.io/BlackjackProject/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/matkins216/BlackjackProject", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
         }
     ]
 }
